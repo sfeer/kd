eefae31181c68ae61eec0abcba5e3fa6e066c323
蔡翔 <caix@longjitech.com>
2021-04-30 17:52:03 +0800

    init commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-export function add(a: number, b: number): number {
-  return a + b
-}
+import KdMenu from './menu'
 
-export function arrayMap(array: [], callback: (item: any, index: number, arr: any[]) => any): any {
-  let i = -1
-  const len = array.length
-  let resArray = []
-  while (++i < len) {
-    resArray.push(callback(array[i], i, array))
-  }
-  return resArray
-}
-
-export function more(a: number, b: number): number {
-  return a * b
+export default {
+  KdMenu
 }
---
+++ src/menu.ts
@@ -0,0 +1,90 @@
+interface KdMenuItem {
+  id: string
+  name: string
+  pid?: string
+  icon?: string
+  url?: string
+  children?: KdMenuItem[]
+}
+
+export default class KdMenu {
+  private path: string = '/'
+  // 高亮菜单
+  private activeMenu: string[] = []
+  // 开启菜单
+  private openMenu: string[] = []
+  // 当前菜单id
+  private cid: string | undefined = undefined
+  private idMap: Map<string, KdMenuItem> = new Map<string, KdMenuItem>()
+  private urlMap: Map<string, KdMenuItem> = new Map<string, KdMenuItem>()
+
+  constructor(menus: KdMenuItem[], path?: string) {
+    this.setPath(path)
+    this.loop(menus, v => {
+      this.idMap.set(v.id, v)
+      v.url && this.urlMap.set(v.url, v)
+    })
+  }
+
+  // 正向递归
+  loop(menus: KdMenuItem[], handle: (arg: KdMenuItem) => void) {
+    menus.forEach(v => {
+      if (v.children) {
+        handle(v)
+        this.loop(v.children, handle)
+      }
+    })
+  }
+
+  // 反向递归
+  loop2(menu: KdMenuItem, handle: (arg: KdMenuItem) => void) {
+    handle(menu)
+    if (menu.pid) {
+      const p = this.idMap.get(menu.pid)
+      p && this.loop2(p, handle)
+    }
+  }
+
+  setPath(path: string | undefined) {
+    if (path) {
+      this.path = path
+      const one = this.urlMap.get(path)
+      if (one) {
+        this.cid = one.id
+        this.loop2(one, v => {
+          this.activeMenu.push(v.id)
+          if (v.pid) {
+            this.openMenu.push(v.pid)
+          }
+        })
+      }
+    } else {
+      this.activeMenu = []
+      this.openMenu = []
+    }
+  }
+
+  getActiveMenu() {
+    return this.activeMenu
+  }
+
+  getOpenMenu() {
+    return this.openMenu
+  }
+
+  /**
+   * 获取当前菜单
+   */
+  getCurrent() {
+    return this.cid ? this.idMap.get(this.cid) : undefined
+  }
+
+  /**
+   * 获取子菜单
+   * @param id
+   */
+  getChild(id: string) {
+    const one = this.idMap.get(id)
+    return one ? one.children : undefined
+  }
+}
