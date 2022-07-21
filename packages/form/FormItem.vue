b604fb5b89539c277597f929cc501f90564a3b0a
蔡翔 <caix@longjitech.com>
2022-07-21 16:26:47 +0800

    图表组件完善

--- packages/form/FormItem.vue
+++ packages/form/FormItem.vue
@@ -1,6 +1,6 @@
 <template>
   <a-form-item :label="model.name">
-    <a-input v-if="type === 'input'" v-model:value="vv" :placeholder="`请选择${model.name}`" v-bind="model.props" />
+    <a-input v-if="type === 'input'" v-model:value="vv" :placeholder="`请输入${model.name}`" v-bind="model.props" />
     <a-input-number v-else-if="type === 'number'" v-model="vv" v-bind="model.props" />
     <a-select
       v-else-if="type === 'select'"
@@ -15,7 +15,7 @@
       :placeholder="`请选择${model.name}`"
       v-model="vv"
     />
-    <a-textarea v-else-if="type === 'textarea'" :auto-size="{ minRows: 2 }" />
+    <a-textarea v-else-if="type === 'textarea'" :placeholder="`请输入${model.name}`" :auto-size="{ minRows: 2 }" />
     <template v-else-if="type === 'text'">{{ vv }}</template>
   </a-form-item>
 </template>
